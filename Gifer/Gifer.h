d13047e729770fcbdf4e56d233d112f4e2158c25
Vladyslav Yakovlev <yakovlev15@icloud.com>
2018-11-12 22:54:57 +0200

    Initial Commit

---
+++ Gifer/Gifer.h
@@ -0,0 +1,19 @@
+//
+//  Gifer.h
+//  Gifer
+//
+//  Created by Vladyslav Yakovlev on 11/12/18.
+//  Copyright © 2018 Vladyslav Yakovlev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Gifer.
+FOUNDATION_EXPORT double GiferVersionNumber;
+
+//! Project version string for Gifer.
+FOUNDATION_EXPORT const unsigned char GiferVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Gifer/PublicHeader.h>
+
+
