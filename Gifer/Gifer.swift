14245ba3fd5fad9a6bd4a7814db0e79f2e5266e5
Vladyslav Yakovlev <yakovlev15@icloud.com>
2019-01-11 23:53:34 +0200

    Added gif creation from images

--- Gifer/Gifer.swift
+++ Gifer/Gifer.swift
@@ -22,6 +22,8 @@ extension Gifer {
         
         case endAddingFramesFailed
         
+        case imagesArrayIsEmpty
+        
         public var errorDescription: String? {
             switch self {
             case .videoUrlInvalid :
@@ -34,6 +36,8 @@ extension Gifer {
                 return "Failed to generate frame"
             case .endAddingFramesFailed :
                 return "Failed to end adding frames"
+            case .imagesArrayIsEmpty :
+                return "Images array is empty"
             }
         }
     }
@@ -41,27 +45,23 @@ extension Gifer {
 
 public final class Gifer {
     
-    private let videoUrl: URL
-    
-    private let frameRate: Int
-    
     private let loopCount: Int
     
-    private let startTime: Float
+    private let startTime: Float!
     
-    private let endTime: Float
+    private let endTime: Float!
     
     private let gifSize: CGSize
     
-    private let videoAsset: AVAsset
+    private let images: [UIImage]!
     
-    private let videoLength: Float
+    private let videoAsset: AVAsset!
+    
+    private let videoLength: Float!
     
     private let delayTime: Float
     
     private init(videoUrl: URL, frameRate: Int, loopCount: Int, startTime: Float?, endTime: Float?, size: CGSize?) {
-        self.videoUrl = videoUrl
-        self.frameRate = frameRate
         self.loopCount = loopCount
         self.videoAsset = AVAsset(url: videoUrl)
         let videoLength = Float(videoAsset.duration.seconds)
@@ -70,18 +70,39 @@ public final class Gifer {
         self.videoLength = videoLength
         self.delayTime = 1.0/Float(frameRate)
         self.gifSize = size ?? .zero
+        self.images = nil
+    }
+    
+    private init(images: [UIImage], frameTime: Float, loopCount: Int, size: CGSize?) {
+        self.images = images
+        self.loopCount = loopCount
+        self.delayTime = frameTime
+        self.gifSize = size ?? .zero
+        self.videoAsset = nil
+        self.videoLength = nil
+        self.startTime = nil
+        self.endTime = nil
     }
     
     public static func createGifFromVideo(_ videoUrl: URL, frameRate: Int = 10, loopCount: Int = 0, startTime: Float? = nil, endTime: Float? = nil, size: CGSize? = nil, completion: @escaping (_ gifUrl: URL?, Error?) -> ()) {
         let gifer = Gifer(videoUrl: videoUrl, frameRate: frameRate, loopCount: loopCount, startTime: startTime, endTime: endTime, size: size)
-        gifer.createGif { gifUrl, error in
+        gifer.createGifFromVideo { gifUrl, error in
             DispatchQueue.main.async {
                 completion(gifUrl, error)
             }
         }
     }
     
-    private func createGif(completion: @escaping (_ gifUrl: URL?, Error?) -> ()) {
+    public static func createGifFromImages(_ images: [UIImage], frameTime: Float, loopCount: Int = 0, size: CGSize? = nil, completion: @escaping (_ gifUrl: URL?, Error?) -> ()) {
+        let gifer = Gifer(images: images, frameTime: frameTime, loopCount: loopCount, size: size)
+        gifer.createGifFromImages { gifUrl, error in
+            DispatchQueue.main.async {
+                completion(gifUrl, error)
+            }
+        }
+    }
+    
+    private func createGifFromVideo(completion: @escaping (_ gifUrl: URL?, Error?) -> ()) {
         if startTime < 0 || endTime > videoLength || startTime >= endTime {
             return completion(nil, .inputParametersInvalid)
         }
@@ -146,5 +167,29 @@ public final class Gifer {
                 CGImageDestinationFinalize(destination) ? completion(fileUrl, nil) : completion(nil, .endAddingFramesFailed)
             }
         }
+    }
+    
+    private func createGifFromImages(completion: @escaping (_ gifUrl: URL?, Error?) -> ()) {
+        guard !images.isEmpty else {
+            return completion(nil, .imagesArrayIsEmpty)
+        }
+        
+        let fileUrl = URL(fileURLWithPath: NSTemporaryDirectory()).appendingPathComponent("gifer.gif")
+        
+        guard let destination = CGImageDestinationCreateWithURL(fileUrl as CFURL, CGImageDestination.gifType, images.count, nil) else {
+            return completion(nil, .destinationFileNotFound)
+        }
+        
+        let fileProperties = [kCGImagePropertyGIFDictionary: [kCGImagePropertyGIFLoopCount: NSNumber(value: Int32(loopCount))], kCGImagePropertyGIFHasGlobalColorMap: NSValue(nonretainedObject: true)] as CFDictionary
+        
+        let frameProperties = [kCGImagePropertyGIFDictionary: [kCGImagePropertyGIFDelayTime: delayTime]] as CFDictionary
+        
+        CGImageDestinationSetProperties(destination, fileProperties)
+        
+        for image in images {
+            CGImageDestinationAddImage(destination, image.cgImage!, frameProperties)
+        }
+        
+        CGImageDestinationFinalize(destination) ? completion(fileUrl, nil) : completion(nil, .endAddingFramesFailed)
     }
 }
